468b6962a2e56ad4aab35eb1bd0291b0e1a260ee
diraoktap <diraoktap.code@gmail.com>
2026-02-16 18:56:40 +0700

    setting fonts, tapi belum selesa

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className='pt-30 grid grid-cols-4 px-4'>
+      <div>
+        <h1 className="font-bold">Dira Okta Prindana</h1>
+        <p>aku adalah seorang developer yang sedang belajar Next.js</p>
+      </div>
+      <div>
+        <h1 style={{ fontFamily: 'var(--font-silkscreen)' }}>DESIGN & <br></br>ENGINEERING</h1>
+      </div>
+      <div>
+        <h1>WHAT I DO</h1>
+        <p>I design and build digital experiences that are both functional and beautiful.</p>
+      </div>
+      <div>
+        <h1>SERVICES</h1>
+        <p>Branding</p>
+        <p>Creative direction & strategy</p>
+        <p>UI/UX design</p>
+        <p>Web development</p>
+        <p>eCommerce, Product, 3D</p>
+        <p>Video & Animation</p>
+      </div>
+    </main >
+  )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,26 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Navbar() {
     return (
-        <nav className="flex gap-4 p-4">
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
-            <Link href="/projects">Projects</Link>
-            <Link href="/contact">Contact</Link>
-        </nav>
+        <main className='fixed left-0 w-full max-w-8xl mx-auto bg-neutral-900/50 backdrop-blur-lg grid grid-cols-4 items-center p-4'>
+            <div>
+                <Link href="/">
+                    <Image
+                        src="/images/logos/logos.svg"
+                        alt="Logo"
+                        className="h-8 w-8"
+                        width={50}
+                        height={50} />
+                </Link>
+            </div>
+            <nav className="col-start-3 flex gap-4">
+                <Link href="/projects">Projects</Link>
+                <Link href="/about">About</Link>
+                <Link href="/dot">D.O.T</Link>
+            </nav>
+            <Link className='flex justify-end' href="/contact">Contact</Link>
+        </main>
+
     )
 }
